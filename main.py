71902f5d402cf837a6b880bd9e5a8fdc712405d6
Jonas Hartwig <fistel@Jonas-Hartwigs-MacBook-Air.local>
2011-12-21 22:49:42 -0800

    flowers can be watered and watercan can be refilled!

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-import pygame, sys
+import pygame, sys, random
 from pygame.locals import *
 
 pygame.init()
@@ -12,13 +12,21 @@ iconsSurfaceObj = pygame.image.load('icons.png')
 iconSize=32
 
 # Create subsurfaces for the 16x16 icons from icons.png. Double them in size to get 32x32 surfaces.
-wateringCanSurfaceObj = pygame.transform.scale2x(iconsSurfaceObj.subsurface(Rect(0,0,16,16)))
-waterSplashSurfaceObj = pygame.transform.scale2x(iconsSurfaceObj.subsurface(Rect(16,0,16,16)))
-flowerSurfaceObj = pygame.transform.scale2x(iconsSurfaceObj.subsurface(Rect(32,0,16,16)))
-flowerstalkSurfaceObj = pygame.transform.scale2x(iconsSurfaceObj.subsurface(Rect(32,16,16,16)))
-sandySurfaceObj = pygame.transform.scale2x(iconsSurfaceObj.subsurface(Rect(0,16,16,16)))
-dirtSurfaceObj = pygame.transform.scale2x(iconsSurfaceObj.subsurface(Rect(0,32,16,16)))
-brickSurfaceObj = pygame.transform.scale2x(iconsSurfaceObj.subsurface(Rect(0,48,16,16))) 
+def getSurfaceFromIcons(x,y):
+    return pygame.transform.scale2x(iconsSurfaceObj.subsurface(Rect(x*16,y*16,16,16)))
+
+wateringCanSurfaceObj = getSurfaceFromIcons(0,0)
+waterSplashSurfaceObj = getSurfaceFromIcons(1,0)
+flowerSurfaceObj = getSurfaceFromIcons(2,0)
+flowerstalkSurfaceObj = getSurfaceFromIcons(2,1)
+pelletSurfaceObj = getSurfaceFromIcons(3,0)
+skullSurfaceObj = getSurfaceFromIcons(4,0)
+tapSurfaceObj = getSurfaceFromIcons(5,0)
+tapSplashSurfaceObj = getSurfaceFromIcons(5,1)
+
+sandySurfaceObj = getSurfaceFromIcons(0,1)
+dirtSurfaceObj = getSurfaceFromIcons(0,2)
+brickSurfaceObj = getSurfaceFromIcons(0,3)
 
 # Create a screen
 screenSize = 16
@@ -33,20 +41,32 @@ soundMove = pygame.mixer.Sound('Move.wav')
 soundRefillWater = pygame.mixer.Sound('RefillWater.wav')
 soundPickup = pygame.mixer.Sound('Pickup.wav')
 
+def drawIcon(surf,x,y):
+    windowSurfaceObj.blit(surf,(x*iconSize,y*iconSize))
 
 class Player:
     xpos = 5  #x position within 32x32 grid
     ypos = screenSize-2  #y position within 32x32 grid
     dir = 1  #1=facing right, -1=facing left
-    screen = 0  # 0 = garden, 1 = outside
+    water = 2000
 
 class Flower:
-    def __init__(self):
-        value = 0
+    height = 1
+    water = 3000
+    xpos = 1
+    def __init__(self,xpos):
+        self.xpos = xpos
+        self.height = random.randint(1,4)
+        self.water = random.randint(2000,4000)
+
+outside = False  # True if outside
 
 player = Player()
 flowers = []
-flowers.append(Flower())
+flowers.append(Flower(random.randint(1,screenSize-2)))
+flowers.append(Flower(random.randint(1,screenSize-2)))
+flowers.append(Flower(random.randint(1,screenSize-2)))
+
 watering = False
 wateringTime = 30
 wateringCounter = wateringTime
@@ -58,14 +78,33 @@ while True:
     
     # draw scene
     for y in range(0,screenSize-1):
-        windowSurfaceObj.blit(brickSurfaceObj, (0,y*iconSize))
-        if (y<screenSize-3):
+        if (y<screenSize-3 or not outside):
+            windowSurfaceObj.blit(brickSurfaceObj, (0,y*iconSize))
+        if (y<screenSize-3 or outside):
             windowSurfaceObj.blit(brickSurfaceObj, ((screenSize-1)*iconSize,y*iconSize))
     for x in range(0,screenSize):
         windowSurfaceObj.blit(dirtSurfaceObj, (x*iconSize, (screenSize-1)*iconSize))
     
-    windowSurfaceObj.blit(flowerstalkSurfaceObj, (3*iconSize, (screenSize-2)*iconSize))
-    windowSurfaceObj.blit(flowerSurfaceObj, (3*iconSize, (screenSize-3)*iconSize))
+
+    if outside:
+        drawIcon(tapSurfaceObj, screenSize-2,screenSize-3)
+    else:
+        for f in flowers:
+            for s in range(1,f.height+1):
+                drawIcon(flowerstalkSurfaceObj,f.xpos,screenSize-1-s)    #draw the stalk
+            drawIcon(flowerSurfaceObj,f.xpos,screenSize-1-(f.height+1))  #draw the flower on top
+            watermeter = fontObj.render(str(f.water/20),False,pygame.Color(20,20,180))
+            waterRect=watermeter.get_rect()
+            waterRect.centerx=f.xpos*iconSize+iconSize/2
+            waterRect.centery=((screenSize-1)*iconSize)+iconSize/2
+            windowSurfaceObj.blit(watermeter,waterRect)
+
+    # update flower data
+    for f in flowers:
+        f.water = max (f.water-1, 0)
+        if f.water<=0:
+            flowers.remove(f)                   #the flower died of dehydration! :(
+            
 
     
     # draw Sandy and her watering can
@@ -81,7 +120,16 @@ while True:
     if watering:
         wateringCounter -= 1
         if (wateringCounter>0):            
-            windowSurfaceObj.blit(pygame.transform.flip(waterSplashSurfaceObj,(player.dir==-1),False),((2*player.dir+player.xpos)*iconSize,player.ypos*iconSize))
+            player.water-=5
+            if (outside and player.xpos==screenSize-3):
+                if wateringCounter%10>5:
+                    drawIcon(tapSplashSurfaceObj, screenSize-2,screenSize-2)
+                    player.water+=50
+            else:    
+                windowSurfaceObj.blit(pygame.transform.flip(waterSplashSurfaceObj,(player.dir==-1),False),((2*player.dir+player.xpos)*iconSize,player.ypos*iconSize))
+                for f in flowers:
+                    if f.xpos == 2*player.dir+player.xpos:
+                        f.water+=5
         else:
             watering=False
         
@@ -92,7 +140,13 @@ while True:
     msgRectObj.centerx = windowSurfaceObj.get_rect().centerx
     msgRectObj.centery = 20
     windowSurfaceObj.blit(msgSurfaceObj, msgRectObj)
-    
+
+    msgSurfaceObj = pygame.transform.scale2x(fontObj.render("Water: "+str(player.water/20),False,pygame.Color(0,0,0)))
+    msgRectObj = msgSurfaceObj.get_rect()
+    msgRectObj.centerx = windowSurfaceObj.get_rect().centerx
+    msgRectObj.centery = 50
+    windowSurfaceObj.blit(msgSurfaceObj, msgRectObj)
+
     # take care of events
     for event in pygame.event.get():
         if event.type == QUIT:
@@ -103,11 +157,23 @@ while True:
             if event.key in (K_LEFT,K_RIGHT):
                 soundMove.play()
                 if event.key == K_LEFT:
-                    player.xpos = max(player.xpos-1,1)
                     player.dir = -1
-                if event.key == K_RIGHT:
-                    player.xpos = min(player.xpos+1,screenSize-2)
+                    player.xpos -= 1
+                    if outside and player.xpos<0:
+                        outside=False
+                        player.xpos=screenSize-1
+                    if not outside and player.xpos<1:
+                        player.xpos=1
+
+            if event.key == K_RIGHT:
                     player.dir = 1
+                    player.xpos+=1
+                    if outside and player.xpos>=screenSize-2:
+                        player.xpos = screenSize-3
+                    if not outside and player.xpos>=screenSize:
+                        outside=True
+                        player.xpos=0
+            
 
             if event.key==K_SPACE:
                 soundRefillWater.play()
