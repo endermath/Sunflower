f611ec2006e15ed91d4491b55e89e380573d8200
Jonas Hartwig <fistel@Jonas-Hartwigs-MacBook-Air.local>
2011-12-21 20:56:39 -0800

    gfx and movement

--- main.py
+++ main.py
@@ -4,18 +4,25 @@ from pygame.locals import *
 pygame.init()
 fpsClock = pygame.time.Clock()
 
-windowSurfaceObj = pygame.display.set_mode((320,320))
 pygame.display.set_caption("Sandy's Sunflowers")
 
 iconsSurfaceObj = pygame.image.load('icons.png')
                            
+
 iconSize=32
+
 # Create subsurfaces for the 16x16 icons from icons.png. Double them in size to get 32x32 surfaces.
 wateringCanSurfaceObj = pygame.transform.scale2x(iconsSurfaceObj.subsurface(Rect(0,0,16,16)))
 waterSplashSurfaceObj = pygame.transform.scale2x(iconsSurfaceObj.subsurface(Rect(16,0,16,16)))
-sandySurfaceObj = pygame.transform.scale2x(iconsSurfaceObj.subsurface(Rect(0,16,16,16)))
 flowerSurfaceObj = pygame.transform.scale2x(iconsSurfaceObj.subsurface(Rect(32,0,16,16)))
 flowerstalkSurfaceObj = pygame.transform.scale2x(iconsSurfaceObj.subsurface(Rect(32,16,16,16)))
+sandySurfaceObj = pygame.transform.scale2x(iconsSurfaceObj.subsurface(Rect(0,16,16,16)))
+dirtSurfaceObj = pygame.transform.scale2x(iconsSurfaceObj.subsurface(Rect(0,32,16,16)))
+brickSurfaceObj = pygame.transform.scale2x(iconsSurfaceObj.subsurface(Rect(0,48,16,16))) 
+
+# Create a screen
+screenSize = 16
+windowSurfaceObj = pygame.display.set_mode((screenSize*iconSize, screenSize*iconSize))
 
 # font
 fontObj = pygame.font.Font('freesansbold.ttf', 16)
@@ -29,7 +36,7 @@ soundPickup = pygame.mixer.Sound('Pickup.wav')
 
 class Player:
     xpos = 5  #x position within 32x32 grid
-    ypos = 5  #y position within 32x32 grid
+    ypos = screenSize-2  #y position within 32x32 grid
     dir = 1  #1=facing right, -1=facing left
     screen = 0  # 0 = garden, 1 = outside
 
@@ -41,18 +48,43 @@ player = Player()
 flowers = []
 flowers.append(Flower())
 watering = False
+wateringTime = 30
+wateringCounter = wateringTime
 
 while True:
     
     # fill background
-    windowSurfaceObj.fill(pygame.Color(70,70,70))
+    windowSurfaceObj.fill(pygame.Color(38,94,179))
+    
+    # draw scene
+    for y in range(0,screenSize-1):
+        windowSurfaceObj.blit(brickSurfaceObj, (0,y*iconSize))
+        if (y<screenSize-3):
+            windowSurfaceObj.blit(brickSurfaceObj, ((screenSize-1)*iconSize,y*iconSize))
+    for x in range(0,screenSize):
+        windowSurfaceObj.blit(dirtSurfaceObj, (x*iconSize, (screenSize-1)*iconSize))
+    
+    windowSurfaceObj.blit(flowerstalkSurfaceObj, (3*iconSize, (screenSize-2)*iconSize))
+    windowSurfaceObj.blit(flowerSurfaceObj, (3*iconSize, (screenSize-3)*iconSize))
+
+    
     # draw Sandy and her watering can
-    windowSurfaceObj.blit(sandySurfaceObj, (player.xpos*iconSize,player.ypos*iconSize))
-    windowSurfaceObj.blit(wateringCanSurfaceObj, ((1+player.xpos)*iconSize,player.ypos*iconSize))
+    if (player.dir==1):
+        windowSurfaceObj.blit(sandySurfaceObj, (player.xpos*iconSize,player.ypos*iconSize))
+        windowSurfaceObj.blit(wateringCanSurfaceObj, ((player.dir+player.xpos)*iconSize,player.ypos*iconSize))
+    else:
+        windowSurfaceObj.blit(pygame.transform.flip(sandySurfaceObj,True,False), (player.xpos*iconSize,player.ypos*iconSize))
+        windowSurfaceObj.blit(pygame.transform.flip(wateringCanSurfaceObj,True,False), ((player.dir+player.xpos)*iconSize,player.ypos*iconSize))
+
     
     #if watering
     if watering:
-        windowSurfaceObj.blit(waterSplashSurfaceObj, ((2+player.xpos)*iconSize,player.ypos*iconSize))
+        wateringCounter -= 1
+        if (wateringCounter>0):            
+            windowSurfaceObj.blit(pygame.transform.flip(waterSplashSurfaceObj,(player.dir==-1),False),((2*player.dir+player.xpos)*iconSize,player.ypos*iconSize))
+        else:
+            watering=False
+        
     
     # show some text
     msgSurfaceObj = pygame.transform.scale2x(fontObj.render("Sandy's Sunflowers",False,pygame.Color(0,0,0)))
@@ -68,19 +100,19 @@ while True:
             sys.exit()
         elif event.type==KEYDOWN:
             
-            if event.key in (K_LEFT,K_RIGHT,K_DOWN,K_UP):
+            if event.key in (K_LEFT,K_RIGHT):
                 soundMove.play()
-                if event.key == K_DOWN:
-                    player.ypos +=1
-                if event.key == K_UP:
-                    player.ypos -=1
                 if event.key == K_LEFT:
-                    player.xpos -=1
+                    player.xpos = max(player.xpos-1,1)
+                    player.dir = -1
                 if event.key == K_RIGHT:
-                    player.xpos +=1
+                    player.xpos = min(player.xpos+1,screenSize-2)
+                    player.dir = 1
 
             if event.key==K_SPACE:
                 soundRefillWater.play()
+                wateringCounter=wateringTime
+                watering=True
         
     pygame.display.update()
     fpsClock.tick(30)
